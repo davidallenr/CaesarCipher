0807d71ffe56e33d8c3b86712f9db46da0312447
David Russell <davidallenr@gmail.com>
2022-04-06 10:45:16 -0400

    Mostly Finished

--- cipher.py
+++ cipher.py
@@ -5,13 +5,27 @@ def encrypt(string, shift):
     # store in result
     for index in range(len(string)):
         character = string[index]
-        if (character.isupper()):
+        if (character.isupper() and ord(character) != 32):
             result += chr((ord(character) + shift-65) % 26 + 65)
         # Encrypt lowercase characters in plain text
-        else:
+        elif (ord(character) != 32):
             result += chr((ord(character) + shift - 97) % 26 + 97)
+        else:
+            result += " "
     return result
 
 
 if __name__ == "__main__":
-    print(encrypt("David", 3))
+    print("Hello, Welcome to the Caesar Cypher Demo")
+    prompt_string = "Please Input a string to convert:"
+    userinput_string = input(prompt_string)
+    prompt_shift = "Please input a shift key (Default is 10)"
+    userinput_shift = int(input(prompt_shift))
+    print("Original String: " + userinput_string)
+    print("Your encrpted String: " + encrypt(userinput_string, userinput_shift))
+
+    print("\n\n")
+    prompt_name = "Please input your full name: "
+    user_name = input(prompt_name)
+    print("Caesar Shift of your name: " + encrypt(user_name, 1))
+    print("Can you figure out the shift key?")
