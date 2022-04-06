c42599d5b38e582f3c6d5a54798052d279a69adc
David Russell <davidallenr@gmail.com>
2022-04-06 10:26:15 -0400

    Encryption working

--- cipher.py
+++ cipher.py
@@ -1,7 +1,17 @@
 def encrypt(string, shift):
     result = ""
+    # Parse string by lenght
+    # add shift to each char
+    # store in result
+    for index in range(len(string)):
+        character = string[index]
+        if (character.isupper()):
+            result += chr((ord(character) + shift-65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(character) + shift - 97) % 26 + 97)
     return result
 
 
 if __name__ == "__main__":
-    print(encrypt("david", 3))
+    print(encrypt("David", 3))
